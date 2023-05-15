ff1601749a3a1ffa47c20c7f8342c75b5f4539d6
Olivier Penhoat <openhoat@gmail.com>
2023-05-15 16:16:47 +0200

    [BUILD] Add HTTP methods

---
+++ http/http_method.ts
@@ -0,0 +1,11 @@
+import type { HttpMethod } from '../types/router.d.ts'
+
+export const httpMethods: HttpMethod[] = [
+  'DELETE',
+  'GET',
+  'HEAD',
+  'OPTIONS',
+  'PATCH',
+  'POST',
+  'PUT',
+]
