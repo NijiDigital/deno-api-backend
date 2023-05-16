a19d764a3ec9e357e8c0dbd22141557359b61fb5
Olivier Penhoat <openhoat@gmail.com>
2023-05-16 08:45:34 +0200

    [FEAT] Add GET /tasks route

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 import { serve } from './deps.ts'
+import { getTasks } from './model/task.ts'
 
-await serve(
-  () => Response.json({ message: 'Resource not found' }, { status: 404 }),
-  { port: 8000 },
-)
+await serve(async (req) => {
+  const { pathname } = new URL(req.url)
+  if (req.method === 'GET' && pathname === '/tasks') {
+    return Response.json(getTasks())
+  }
+  return Response.json({ message: 'Resource not found' }, {
+    status: 404,
+  })
+}, { port: 8000 })
